fb1f08475ab4c36deb0bc1e9fe80239ab18a1862
IkhtiyorDev <ikhtiyordev@gmail.com>
2021-10-05 14:20:32 +0500

    Create aws-s3-example project

---
+++ src/main/java/dev/ikhtiyor/awss3example/AwsS3ExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.awss3example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3ExampleApplication.class, args);
+    }
+
+}
